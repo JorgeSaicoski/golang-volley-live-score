b91408cfeb93b764112926f799b3b9d8c1f93bfb
Jorge Sarkis <jorgesaicoski@gmail.com>
2024-09-04 22:17:17 -0300

    routes update

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -7,4 +7,7 @@ import (
 
 func SetupRoutes(r *gin.Engine) {
 	r.GET("/matches", handlers.GetMatches)
+	r.GET("/matches/:id", handlers.GetMatchByID)
+	r.POST("/matches", handlers.CreateMatch)
+	r.PUT("/matches/:id", handlers.UpdateMatch)
 }
