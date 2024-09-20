e3c04dce0d2b1e3a1bed4de9e2e3f0378ed2026b
Jorge Sarkis <jorgesaicoski@gmail.com>
2024-09-19 21:42:52 -0300

    bug routes fix

--- internal/matches/routes/routes.go
+++ internal/matches/routes/routes.go
@@ -1,13 +1,13 @@
 package routes
 
 import (
-	handlers_matches "github.com/JorgeSaicoski/golang-volley-live-score/internal/handlers"
+	"github.com/JorgeSaicoski/golang-volley-live-score/internal/matches/handlers"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(r *gin.Engine) {
-	r.GET("/matches", handlers_matches.GetMatches)
-	r.GET("/matches/:id", handlers_matches.GetMatchByID)
-	r.POST("/matches", handlers_matches.CreateMatch)
-	r.PUT("/matches/:id", handlers_matches.UpdateMatch)
+	r.GET("/matches", handlers.GetMatches)
+	r.GET("/matches/:id", handlers.GetMatchByID)
+	r.POST("/matches", handlers.CreateMatch)
+	r.PUT("/matches/:id", handlers.UpdateMatch)
 }
